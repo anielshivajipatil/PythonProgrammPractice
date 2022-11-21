62c25dfb9cc7f230f159c641c6f47428b30514b6
Anil <anilshivajipatil@yahoo.in>
2022-11-21 12:45:22 +0530

    added motivation phrase

--- greetings.py
+++ greetings.py
@@ -1,1 +1,9 @@
 Print("Welcome!")
+Print("Good Bye")
+Print("Welcome My Friend!")
+
+# show some cool Greetings Image
+
+# Ask the user what he/she wants to drink
+
+if you can dream it -you can do it!
